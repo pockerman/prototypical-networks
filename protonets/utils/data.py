725a954f398344e8926de7f0a417dca2a313435a
Alex <a.giavaras@gmail.com>
2021-01-21 10:19:31 +0000

    Add tuf data

--- protonets/utils/data.py
+++ protonets/utils/data.py
@@ -1,9 +1,15 @@
 import protonets.data
 
+
 def load(opt, splits):
     if opt['data.dataset'] == 'omniglot':
         ds = protonets.data.omniglot.load(opt, splits)
+    elif opt['data.dataset'] == 'tuf':
+        ds = protonets.data.tuf.load(opt=opt, splits=splits)
     else:
         raise ValueError("Unknown dataset: {:s}".format(opt['data.dataset']))
 
     return ds
+
+
+
