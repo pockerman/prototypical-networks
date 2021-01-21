92de31e7b85f89e63163add9641a5c168393e298
Alex <a.giavaras@gmail.com>
2021-01-21 10:17:26 +0000

    Add clarifying exception

--- protonets/models/factory.py
+++ protonets/models/factory.py
@@ -1,5 +1,6 @@
 MODEL_REGISTRY = {}
 
+
 def register_model(model_name):
     def decorator(f):
         MODEL_REGISTRY[model_name] = f
@@ -7,7 +8,12 @@ def register_model(model_name):
 
     return decorator
 
+
 def get_model(model_name, model_opt):
+
+    if len(MODEL_REGISTRY) == 0:
+        raise ValueError("Model registry is empty")
+
     if model_name in MODEL_REGISTRY:
         return MODEL_REGISTRY[model_name](**model_opt)
     else:
